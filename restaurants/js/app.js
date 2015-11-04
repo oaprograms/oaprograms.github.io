4c1c17bdd394cdea64670f2761043fe2eabf00b9
Ognjen Apic <ognjen.apic@gmail.com>
2015-11-04 18:57:46 +0100

    initial

--- restaurants/js/app.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-
-var app = angular.module("app", ['restangular', 'ui.bootstrap']);
-
---
+++ static/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    var owl = {
+        singleItem: true,
+        autoPlay: 4000,
+        autoWidth:true,
+        autoHeight:true
+    };
+	$("#owl1").owlCarousel(owl);
+    $("#owl2").owlCarousel(owl);
+    $("#owl3").owlCarousel(owl);
+});
