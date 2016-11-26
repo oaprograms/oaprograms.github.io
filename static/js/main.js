fbff05bd529eb30fe7e077c9048ed75f032fd2ac
Ognjen Apic <ognjen.apic@gmail.com>
2016-11-26 07:57:48 +0100

    portfolio update

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,25 @@
 $(document).ready(function() {
-    var owl = {
+    var $owl = $('.owl');
+	$owl.owlCarousel({
         singleItem: true,
-        autoPlay: 4000,
+        autoPlay: 3000,
         autoWidth:true,
         autoHeight:true
-    };
-	$("#owl1").owlCarousel(owl);
-    $("#owl2").owlCarousel(owl);
-    $("#owl3").owlCarousel(owl);
-    $("#owl4").owlCarousel(owl);
+    });
+    $(window).scroll(function(){
+        $owl.each(function(){
+            var $this = $(this);
+            var owl = $this.data('owlCarousel');
+            if ($this.offset().top > $(window).scrollTop() - 50 && $this.offset().top + $this.height() < $(window).scrollTop() + $(window).height() + 50){
+                if(!$this.hasClass('playing')){
+                    $this.addClass('playing');
+                    owl.play();
+                }
+            } else {
+                $this.removeClass('playing');
+                owl.stop();
+            }
+        })
+    });
+
 });
